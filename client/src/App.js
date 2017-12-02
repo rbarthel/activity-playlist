380b16911aa121ed3d43d1290e609e4a2facdbde
NatalieMok <nataliemok__@hotmail.com>
2017-12-02 14:14:32 -0800

    add search functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import jQuery from 'jquery';
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar"
 import spotify from './images/spotify.png';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+   this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    if (!this.state.value) {
+      alert('Please enter a value to search');
+    } else {
+     jQuery.ajax({
+        url: `https://localhost:8080/search/playlists/${this.state.value}`,
+        dataType: 'json',
+        success: function(data) {
+          console.log(data);
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    }
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,7 +48,7 @@ class App extends Component {
             To get started, what kind of activity are you doing?
           </p>
 
-          <SearchBar />
+          <SearchBar handleSubmit={this.handleSubmit} handleChange={this.handleChange} value={this.state.value} />
         </div>
       </div>
     );
