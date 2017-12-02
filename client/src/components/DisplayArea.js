7cd3886cb3f4229610859bfee2dbc0927f4e4770
NatalieMok <nataliemok__@hotmail.com>
2017-12-02 15:36:59 -0800

    show tracks on search

---
+++ client/src/components/DisplayArea.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import SearchBar from "./SearchBar";
+import TrackList from "./TrackList";
+
+class DisplayArea extends Component {
+  render() {
+    if (this.props.tracks === false) {
+      return (<SearchBar handleSubmit={this.props.handleSubmit} handleChange={this.props.handleChange} value={this.props.value} />)
+    } else {
+      return (<TrackList tracks={this.props.tracks} />)
+    }
+  }
+}
+export default DisplayArea;
